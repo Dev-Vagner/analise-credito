1b8e991c5675538b93a3675268b6dbbdfca95b10
vagner.barbosa <vagner.barbosa@cwi.com.br>
2024-11-18 08:30:18 -0300

    Fiz pequenos ajustes no serviço de notificação e analise de credito

--- analisecredito/src/main/java/br/com/vbruno/analisecredito/AnalisecreditoApplication.java
+++ analisecredito/src/main/java/br/com/vbruno/analisecredito/AnalisecreditoApplication.java
@@ -1,15 +1,10 @@
 package br.com.vbruno.analisecredito;
 
-import br.com.vbruno.analisecredito.service.analisecredito.AnaliseCreditoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnalisecreditoApplication {
-
-	@Autowired
-	private AnaliseCreditoService analiseCreditoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnalisecreditoApplication.class, args);
--- notificacao/src/main/java/br/com/vbruno/notificacao/listener/PropostaConcluidaListener.java
+++ notificacao/src/main/java/br/com/vbruno/notificacao/listener/PropostaConcluidaListener.java
@@ -8,14 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PropostaPendenteListener {
+public class PropostaConcluidaListener {
 
     @Autowired
     private NotificacaoEmailService notificacaoEmailService;
 
-    @RabbitListener(queues = "${rabbitmq.proposta-pendente.queue}")
-    public void propostaPendente(Proposta proposta) {
-        String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+    @RabbitListener(queues = "${rabbitmq.proposta-concluida.queue}")
+    public void propostaConcluida(Proposta proposta) {
+        String mensagem = String.format(MensagemConstante.PROPOSTA_CONCLUIDA, proposta.getUsuario().getNome());
         notificacaoEmailService.notificar(proposta.getUsuario().getEmail(), mensagem);
     }
 }
